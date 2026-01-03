2734e7972b03602ed3ff79f73b30900c9776df15
adavilalith <adavilalith11@gmail.com>
2026-01-04 00:33:15 +0530

    changing postgres unit test to refelct changes in table structure

--- tests/unit/test_postgres_client.py
+++ tests/unit/test_postgres_client.py
@@ -4,8 +4,8 @@ import pytest
 def test_save_jobs_insertion(db_client):
     """Test that jobs are successfully inserted into the DB"""
     sample_jobs = [
-        {"job_id": "test_1", "title": "Data Analyst", "company": "TechCorp"},
-        {"job_id": "test_2", "title": "BI Engineer", "company": "DataInc"}
+        {"title": "Data Analyst", "company": "TechCorp"},
+        {"title": "BI Engineer", "company": "DataInc"}
     ]
     
     db_client.save_jobs(sample_jobs, "Data Analyst", "Hyderabad, Telangana, India")
@@ -15,18 +15,3 @@ def test_save_jobs_insertion(db_client):
         count = cur.fetchone()[0]
     
     assert count == 2
-
-@pytest.mark.unit
-def test_upsert_logic(db_client):
-    """Test that duplicate job_ids do not create duplicate rows"""
-    duplicate_job = [{"job_id": "unique_123", "title": "Duplicate Test"}]
-    
-    # Run insertion twice
-    db_client.save_jobs(duplicate_job, "Tester", "Remote")
-    db_client.save_jobs(duplicate_job, "Tester", "Remote")
-    
-    with db_client.conn.cursor() as cur:
-        cur.execute("SELECT count(*) FROM jobs_raw WHERE job_id = 'unique_123';")
-        count = cur.fetchone()[0]
-    
-    assert count == 1  # Should still be 1 despite two calls
